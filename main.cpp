db9a0c94a07e049ec60f9d1c57691bc4703f6253
vortamds <vortamds@gmail.com>
2026-06-26 16:07:08 +0330

    update0.0.2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#define DEBUG
+// #define RELEASE
 
 int main() {
 
-    std::cout << "Hello Milad!\n";
+    #ifdef DEBUG
+        std::cout << "حالت دیباگ فعال هست !\n";
+    #endif
+    #ifndef RELEASE
+        std::cout << "این نسخه release نیست \n";
+    #endif
+
+    #if 0
+        std::cout << "این خط چاپ نمیشو\n";
+        //حتی این 
+        std::cout << "حتی این ";
+    #endif
+
 
     return 0;
+
 }
