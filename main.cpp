5176cc987540dc6df855bc2be2512e791fb12971
vortamds <vortamds@gmail.com>
2026-07-02 10:37:35 +0330

    update0.0.7

--- main.cpp
+++ main.cpp
@@ -1,15 +1,10 @@
 // main.cpp
 #include <iostream>
-int getUserInput() {
-    std::cout << "Enter an number: ";
-    int x{};
-    std::cin >> x;
-    return x;
-}
-int main() 
-{
-    int value{getUserInput()};
-    std::cout << "عددی که وارد کردید " << value << " است \n";
 
+int main() {
+    int a = 13;
+    int b = 10;
+    int sum = a + b;
+    std::cout << "Sum is: " << sum << '\n';
     return 0;
 }
