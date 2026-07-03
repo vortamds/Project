96fce054b5338af9edbba7b99a4526e105f32c83
vortamds <vortamds@gmail.com>
2026-07-03 19:11:31 +0330

    update0.0.8

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 // main.cpp
 #include <iostream>
 
+void a() {
+    std::cout << "a() called\n";
+}
+void b() {
+    std::cout << "b() called\n";
+    a();
+}
 int main() {
-    int a = 13;
-    int b = 10;
-    int sum = a + b;
-    std::cout << "Sum is: " << sum << '\n';
-    return 0;
+    a();
+    b();
 }
