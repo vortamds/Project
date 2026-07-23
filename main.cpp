c1c90035857841c6928ea25aff03cc81819302d3
vortamds <vortamds@gmail.com>
2026-07-23 13:52:15 +0330

    4.9

--- main.cpp
+++ main.cpp
@@ -2,22 +2,12 @@
 
 int main() {
 
-    double num1 , num2;
-    std::cout << "Enter a nubmer one : ";
-    std::cin >> num1;
-    std::cout << "Enter a number two : ";
-    std::cin >> num2;
+    bool isSunny {true};
+    bool isCloudy {!isSunny};
 
-    double sum = num1 + num2;
-    double diff = num1 - num2;
-    double product = num1 * num2;
-    double quotient = num1 / num2;
+    std::cout << "افتابی:‌" << isSunny << '\n';
+    std::cout << "ابری :‌" << isCloudy << '\n';
 
-
-    std::cout << "جمع این دو عدد :‌" << sum << '\n';
-    std::cout << "تفریق این دو عدد :‌" << diff << '\n';
-    std::cout << "ضرب این دو عدد :‌" << product << '\n';
-    std::cout << "تقسیم این دو عدد :‌" << quotient << '\n';
 
     return 0;
 
