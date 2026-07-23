bc42656508abbb921d0efa463f1f8b96348e1472
vortamds <vortamds@gmail.com>
2026-07-23 13:43:21 +0330

    code1

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
-// main.cpp
 #include <iostream>
-int main(){
 
-    std::cout << "size of char: " << sizeof(char) << '\n';
-    std::cout << "size of int: " << sizeof(int) << '\n';
-    std::cout << "size of float: " << sizeof(float) << '\n';
-    std::cout << "size of couble: " << sizeof(double) << '\n';
+int main() {
 
+    double num1 , num2;
+    std::cout << "Enter a nubmer one : ";
+    std::cin >> num1;
+    std::cout << "Enter a number two : ";
+    std::cin >> num2;
+
+    double sum = num1 + num2;
+    double diff = num1 - num2;
+    double product = num1 * num2;
+    double quotient = num1 / num2;
+
+
+    std::cout << "جمع این دو عدد :‌" << sum << '\n';
+    std::cout << "تفریق این دو عدد :‌" << diff << '\n';
+    std::cout << "ضرب این دو عدد :‌" << product << '\n';
+    std::cout << "تقسیم این دو عدد :‌" << quotient << '\n';
 
     return 0;
+
+
 }
